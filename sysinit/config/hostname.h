c88d71612af00ed2f264feec5e4af5e1d52772c2
Peter Johansson <zeronightfall@gmx.com>
2010-01-11 23:10:59 +0100

    Added code to handle hostname.

---
+++ sysinit/config/hostname.h
@@ -0,0 +1,1 @@
+const char hostname[] = "twilight";
--- sysinit/hostname.c
+++ sysinit/hostname.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
 #include "hostname.h"
+#include "config/hostname.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 void hostname_init(void)
 {
-    printf("hostname init\n");
+    
+    sethostname(hostname, strlen(hostname)); 
 }
 
 void hostname_shutdown(void)
 {
-    printf("hostname shutdown\n");
 }
