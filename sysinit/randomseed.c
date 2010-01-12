1b2e415d84e79c2e5c84973b82430648d3f9e8ac
Peter Johansson <zeronightfall@gmx.com>
2010-01-12 01:41:10 +0100

    Tested the run functionality.

--- sysinit/randomseed.c
+++ sysinit/randomseed.c
@@ -1,9 +1,13 @@
 #include "randomseed.h"
 #include "config/randomseed.h"
+#include "../lib/run.h"
 #include <stdio.h>
+
 
 void randomseed_init(void)
 {
+    char* const argv[] = {RANDOMSEED, ">", "/dev/urandom", NULL};
+    run("/bin/cat", argv);
 }
 
 void randomseed_shutdown(void)
