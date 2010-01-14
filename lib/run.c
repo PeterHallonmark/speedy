aace708d9eba6d48eed216f2753706d9cb9976d9
Peter Johansson <zeronightfall@gmx.com>
2010-01-14 01:29:44 +0100

    run.c is also using simulatted functions.

--- lib/run.c
+++ lib/run.c
@@ -2,21 +2,25 @@
 
 #include <stdio.h>
 #include <sys/types.h>
+
+#ifdef SIMULATE
+#include "simulate.h"
+#else
 #include <sys/wait.h>
 #include <unistd.h>
+#endif
 
 void run(const char *filename, char *const argv[])
 {
     pid_t pid = fork();
     int exec_return;
     int status;
-    char* const env[] = {NULL};
     
     /* Check if it is the child. */
     if (pid == 0) {
-        exec_return = execve(filename, argv, env);
+        exec_return = execv(filename, argv);
         /* If execve fails. */
-        printf("Failure! execve error code = %d\n", exec_return);
+        printf("Failure! execv error code = %d\n", exec_return);
         _exit(0); 
     } else if (pid < 0) {
         printf("Failed to fork\n");
