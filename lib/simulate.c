6243165a16ab9f4b2888ebb32c6f43d47390bee8
Peter Johansson <zeronightfall@gmx.com>
2010-01-14 01:28:07 +0100

    Added functions to be able to simulate fork, wait, system and execv.

--- lib/simulate.c
+++ lib/simulate.c
@@ -7,3 +7,23 @@ void sethostname(const char* hostname, int size)
 {
     printf("Hostname = %s\n", hostname);
 }
+
+void system(const char *command)
+{
+    printf("Command = %s\n", command);
+}
+
+int execv(const char *path, char *const argv[])
+{
+    printf("execv = %s",path);
+    return 0;
+}
+
+int fork(void)
+{
+    return 0;
+}
+
+void wait(int *status)
+{
+}
--- lib/simulate.h
+++ lib/simulate.h
@@ -1,1 +1,10 @@
 void sethostnamex(const char* hostname, int size);
+
+void system(const char *command);
+
+
+int execv(const char *path, char *const argv[]);
+
+int fork(void);
+
+void wait(int *status);
