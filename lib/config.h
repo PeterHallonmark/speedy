25f3fdd99eeaab9ed866c54258cee1dc14468117
Peter Johansson <zeronightfall@gmx.com>
2010-02-14 02:04:10 +0100

    Added license information and added a couple of defines.

--- lib/config.h
+++ lib/config.h
@@ -1,3 +1,19 @@
+/*
+    Copyright (c) 2010, Peter Johansson <zeronightfall@gmx.com>
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose with or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
 #define CMD_MKNOD "/bin/mknod"
 
 #define CMD_LN "/bin/ln"
@@ -28,4 +44,40 @@
 
 #define CMD_LOADKEYS "/bin/loadkeys"
 
+#define CMD_DEPMOD "/sbin/depmod"
+
+#define FILE_HWCLOCK_ADJTIME "/var/lib/hwclock/adjtime"
+
+#define FILE_LOCALTIME "/etc/localtime"
+
+#define FILE_RANDOMSEED "/var/lib/misc/random-seed"
+
+#define FILE_NOLOGIN "/etc/nologin"
+
+#define FILE_FORCECHK "/forcefsck"
+
+#define FILE_SHUTDOWNPID "/etc/shutdownpid"
+
+#define FILE_UTMP "/var/run/utmp"
+
+#define FILE_PROFILE_LOCALE "/etc/profile.d/locale.sh"
+
 #define PATH_DEV "/dev"
+
+#define PATH_ZONEINFO "/usr/share/zoneinfo"
+
+#define PATH_LOCK "/var/lock"
+
+#define PATH_TMP "/tmp"
+
+#define PATH_RUN "/var/run"
+
+#define DIR_TMP_ICE "/tmp/.ICE-unix"
+
+#define DIR_TMP_X11 "/tmp/.X11-unix"
+
+#define DEV_RTC "/dev/rtc"
+
+#define DEV_RTC_0 DEV_RTC"0"
+
+#define KERNEL_HOTPLUG "/proc/sys/kernel/hotplug"
